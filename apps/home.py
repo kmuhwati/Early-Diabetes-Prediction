7151c00e16b9f1ac511c8e216318cbb712677195
Kumbirai Muhwati <kmuhwati@mail.yu.edu>
2024-07-30 14:02:41 -0400

    Add files via upload

---
+++ apps/home.py
@@ -0,0 +1,32 @@
+# pip install openpyxl
+import pandas as pd
+import streamlit as st
+import zipfile
+import base64
+import os
+from PIL import Image
+from streamlit_lottie import st_lottie
+import json
+import requests
+
+
+
+
+
+def app():
+    st.title('Home')
+    st.markdown("""
+                ## Early Diabetes Detection Machine learning App By Tapiwa Chamboko.
+                
+
+
+
+---
+""")
+
+    st.write('')
+
+    st.write('Welcome to Diabetes prediction App')
+    st.image('1.png', width=800)
+    
+  
